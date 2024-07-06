d9197940be8f4a7403ece044b9a332c4186cec82
AhmedShahan <shahan.ahmed@northsouth.edu>
2024-07-06 22:14:56 +0600

    basic python list upto list pop and remove

--- 1_basic.py
+++ 1_basic.py
@@ -1,27 +1,28 @@
-# This is single line comment1
-# This is sinle line comment 2
+# # This is single line comment1
+# # This is sinle line comment 2
 
-'''
-This is multiline comment
-'''
+# '''
+# This is multiline comment
+# '''
 
-print("Hello World")
-print("Hello Python")
+# print("Hello World")
+# print("Hello Python")
 
-print(12,3,4,5)
+# print(12,3,4,5)
 
-a=input("Enter Number 1: ")
-b=input("Enter number 2: ")
+# a=input("Enter Number 1: ")
+# b=input("Enter number 2: ")
 
-a=int(a)
-b=int(b)
-print("You Have Entered")
-print("Number A = ",a)
-print("Number B = ",b)
+# a=int(a)
+# b=int(b)
+# print("You Have Entered")
+# print("Number A = ",a)
+# print("Number B = ",b)
 
-print("Sum = ",a+b)
-print("Sub = ",a-b)
-print("Multiplication = ",a*b)
-print("Divsion = ",a/b)
+# print("Sum = ",a+b)
+# print("Sub = ",a-b)
+# print("Multiplication = ",a*b)
+# print("Divsion = ",a/b)
+
 
 
---
+++ 2_list.py
@@ -0,0 +1,26 @@
+fruits=['apple', 'orange', 'banana']
+
+# print(fruits)
+
+'''
+fruits=   0        1       2
+        apple   orange  banana
+'''
+
+# print(fruits[1])
+
+#### max, min of list
+age_list=[45,37,23,55,25,50]
+# print(max(age_list))
+# print(min(age_list))
+
+fruits=['apple', 'orange', 'banana']
+
+fruits.append("Cherry")
+print(fruits)
+
+fruits.pop()
+print(fruits)
+
+fruits.remove("orange")
+print(fruits)
