7a6feac9cc538c0e8228b1e1f072b41b25960d5b
AhmedShahan <shahan.ahmed@northsouth.edu>
2024-07-06 22:20:34 +0600

    basic python slicing

--- 2_list.py
+++ 2_list.py
@@ -14,13 +14,30 @@ age_list=[45,37,23,55,25,50]
 # print(max(age_list))
 # print(min(age_list))
 
-fruits=['apple', 'orange', 'banana']
+# fruits=['apple', 'orange', 'banana']
 
-fruits.append("Cherry")
-print(fruits)
+# fruits.append("Cherry")
+# print(fruits)
 
-fruits.pop()
-print(fruits)
+# fruits.pop()
+# print(fruits)
 
-fruits.remove("orange")
-print(fruits)
+# fruits.remove("orange")
+# print(fruits)
+
+
+
+### slicing
+fruits=["apple", "banana", "charry", "Mango", "Jackfruit","Litchi"]
+'''
+            0       1           2       3           4       5
+fruits=["apple", "banana", "charry", "Mango", "Jackfruit","Litchi"]
+
+'''
+
+print(fruits[0:6])  # Slice from index 0 to 4   = "apple", "banana", "charry", "Mango", "Jackfruit","Litchi"
+print(fruits[0:])   # slice from 0 to last      = "apple", "banana", "charry", "Mango", "Jackfruit","Litchi"
+
+print(fruits[1:4])  #slice from index 1 to 3    = "banana", "charry", "Mango"
+
+print(fruits[:4])   # slice from 0 to 3         = "apple", "banana", "charry", "Mango"
