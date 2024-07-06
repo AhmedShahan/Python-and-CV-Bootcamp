8f2ab3faba2ab2d5c382a3fc70225ba53218d3ba
AhmedShahan <shahan.ahmed@northsouth.edu>
2024-07-06 22:05:21 +0600

    basic python upto basic calculation

--- 1_basic.py
+++ 1_basic.py
@@ -4,3 +4,24 @@
 '''
 This is multiline comment
 '''
+
+print("Hello World")
+print("Hello Python")
+
+print(12,3,4,5)
+
+a=input("Enter Number 1: ")
+b=input("Enter number 2: ")
+
+a=int(a)
+b=int(b)
+print("You Have Entered")
+print("Number A = ",a)
+print("Number B = ",b)
+
+print("Sum = ",a+b)
+print("Sub = ",a-b)
+print("Multiplication = ",a*b)
+print("Divsion = ",a/b)
+
+
